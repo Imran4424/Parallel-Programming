78bfe3cc33bdd72e281618cf2d0ad920d0bb48b2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-01 00:41:19 +0600

    Parallel programming codes

--- Java/Addition using rmi/Adder.java
+++ Java/Addition using rmi/Adder.java
@@ -1,6 +1,10 @@
 import java.rmi.remote;
 
-public interface Adder extends remote
+public interface Adder extends Remote
 {
-	
+	//declaring the method prototype
+
+	public int add(int x,int y)
+
+	throws java.rmi.RemoteException;
 }
