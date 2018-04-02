e2efaca57de1db473fc119c0a14aef8043a48d97
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-02 21:16:26 +0600

    java rmi codes

--- Java/Addition using rmi/Adder.java
+++ Java/Addition using rmi/Adder.java
@@ -4,7 +4,5 @@ public interface Adder extends Remote
 {
 	//declaring the method prototype
 
-	public int add(int x,int y)
-
-	throws java.rmi.RemoteException;
+	public int add(int x,int y) throws java.rmi.RemoteException;
 }
