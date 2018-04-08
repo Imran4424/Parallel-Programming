c59700119630ce29db502aeb8fc36fed3871992c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-08 10:23:20 +0600

    parallel programming codes

---
+++ Java/Adding a image using rmi 3 pices/ImageProcessor.java
@@ -0,0 +1,16 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+ /**
+ * ImageProcessor
+ */
+
+public interface ImageProcessor extends Remote
+{
+    /**
+     * Declaring the method prototype
+     */
+
+     
+    
+}
