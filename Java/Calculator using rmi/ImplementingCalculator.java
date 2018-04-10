27db5cc386eb90122545537dca2c7684ec281cad
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-10 16:10:43 +0600

    parallel programming

--- Java/Calculator using rmi/ImplementingCalculator.java
+++ Java/Calculator using rmi/ImplementingCalculator.java
@@ -1,10 +1,10 @@
-
-
+import java.rmi.server.UnicastRemoteObject;
 
 /**
  * ImplementingCalculator
  */
-public class ImplementingCalculator  {
+public class ImplementingCalculator extends UnicastRemoteObject implements Calculator 
+{
 
     
 }
