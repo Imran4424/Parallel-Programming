662ed14eb1eaadd3fc584f4587b147b8a945742e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-10 19:29:05 +0600

    parallel programming

--- Java/Calculator using rmi/CalculatorClient.java
+++ Java/Calculator using rmi/CalculatorClient.java
@@ -47,7 +47,7 @@ public class CalculatorClient
         {
             System.out.println();
             System.out.println("Not Bound Exception");
-            System.out.println();
+            System.out.println(notbe);
         }
     }
     
