f714c94a9adad846ba4b553b84ea99ede9e1a568
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-10 17:51:51 +0600

    parallel programming

---
+++ Java/Calculator using rmi/CalculatorClient.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * CalculatorClient
+ */
+
+public class CalculatorClient 
+{
+    public static void main(String[] args) 
+    {
+        int x = Integer.parseInt(args[0]);
+    }
+    
+}
