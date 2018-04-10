41048096effac31d1f35597efa4e78cdeafdcecd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-10 19:06:01 +0600

    parallel programming

--- Java/Calculator using rmi/CalculatorClient.java
+++ Java/Calculator using rmi/CalculatorClient.java
@@ -20,7 +20,7 @@ public class CalculatorClient
             
             System.out.println("The addition of " + x + " and " + y + " is: " + obj.add(x, y));
 
-            System.out.println("The substraction of " + x + " and " + y + " is: ");
+            System.out.println("The substraction of " + x + " and " + y + " is: " + obj.sub(x, y));
         } 
         catch (Exception e) {
             //TODO: handle exception
