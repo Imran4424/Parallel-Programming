b3708d83a986cb0631253a2fc56613c4dd8fac34
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-10 19:19:56 +0600

    parallel programming

--- Java/Calculator using rmi/CalculatorClient.java
+++ Java/Calculator using rmi/CalculatorClient.java
@@ -40,7 +40,7 @@ public class CalculatorClient
         {
             System.out.println();
             System.out.println("Remote Exception");
-            System.out.println("dsad");
+            System.out.println(re);
         }
     }
     
