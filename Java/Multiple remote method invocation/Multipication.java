dfa05f860274174a93aee92f9b257cead51035a7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-19 23:05:20 +0600

    Parallel programming

--- Java/Multiple remote method invocation/Multipication.java
+++ Java/Multiple remote method invocation/Multipication.java
@@ -14,5 +14,20 @@ public class Multipication extends UnicastRemoteObject implements Calculator
     public Multipication() throws RemoteException
     {
         super();
+
+        /**
+        * super keyword use to called parent constructor
+        * super can be used to do more specific work like
+        * 
+        * super(arguments) to called parent constructor who accepts arguments
+        *
+        * super can also use to called parent class method that is  - super.aMethod()
+        *
+        * and we all know that what constructor is, constructure initialize the object values with given values
+        *
+        * for default constructors, the object values will be initialized with default values
+        */
     }
+
+    
 }
