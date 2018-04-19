f01bc1858098948b91d38e1b16e6c711bf3c0a85
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-19 23:31:04 +0600

    Parallel programming

--- Java/Multiple remote method invocation/Division.java
+++ Java/Multiple remote method invocation/Division.java
@@ -4,7 +4,7 @@ import java.rmi.server.UnicastRemoteObject;
  * Division
  */
 
-public class Division extends UnicastRemoteObject implements
+public class Division extends UnicastRemoteObject implements Calculator
 {
 
     
