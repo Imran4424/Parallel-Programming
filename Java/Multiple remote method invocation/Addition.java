ff329a768d2baf8e2b379c3f644d3d6daafb77ea
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-19 22:07:39 +0600

    Parallel programming

--- Java/Multiple remote method invocation/Addition.java
+++ Java/Multiple remote method invocation/Addition.java
@@ -12,22 +12,26 @@ public class Addition extends UnicastRemoteObject implements Calculator
      * in order to throw RemoteException exception
      */
 
-     public Addition() throws RemoteException
-     {
-         super();
+    public Addition() throws RemoteException
+    {
+        super();
 
-         /**
-         * super keyword use to called parent constructor
-         * super can be used to do more specific work like
-         * 
-         * super(arguments) to called parent constructor who accepts arguments
-         *
-         * super can also use to called parent class method that is  - super.aMethod()
-         *
-         * and we all know that what constructor is, constructure initialize the object values with given values
-         *
-         * for default constructors, the object values will be initialized with default values
-         */
-     }
+        /**
+        * super keyword use to called parent constructor
+        * super can be used to do more specific work like
+        * 
+        * super(arguments) to called parent constructor who accepts arguments
+        *
+        * super can also use to called parent class method that is  - super.aMethod()
+        *
+        * and we all know that what constructor is, constructure initialize the object values with given values
+        *
+        * for default constructors, the object values will be initialized with default values
+        */
+    }
+
+    
+    
+    
     
 }
