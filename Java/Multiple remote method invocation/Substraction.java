30cf8ddd3013fbe3aabcb7e5a2e125415f194f73
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-19 22:30:02 +0600

    Parallel programming

--- Java/Multiple remote method invocation/Substraction.java
+++ Java/Multiple remote method invocation/Substraction.java
@@ -17,6 +17,17 @@ public class Substraction extends UnicastRemoteObject implements Calculator
     {
         super();
 
-        
+        /**
+        * super keyword use to called parent constructor
+        * super can be used to do more specific work like
+        * 
+        * super(arguments) to called parent constructor who accepts arguments
+        *
+        * super can also use to called parent class method that is  - super.aMethod()
+        *
+        * and we all know that what constructor is, constructure initialize the object values with given values
+        *
+        * for default constructors, the object values will be initialized with default values
+        */
     }
 }
