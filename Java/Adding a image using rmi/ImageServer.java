0fdb350e897ae05bafc60b5076746d4856264582
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-22 00:12:18 +0600

    parallel programming

--- Java/Adding a image using rmi/ImageServer.java
+++ Java/Adding a image using rmi/ImageServer.java
@@ -10,6 +10,10 @@ public class ImageServer
     public ImageServer()
     {
         ImageProcessor obj = new ImplementingImageProcessor();
+
+        String url = "rmi://localhost:1099/ImageService";
+
+        
     }
     
 }
