53fb9ad27f33bad8641ea7d0d7da6dfa11f21134
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-22 00:39:40 +0600

    parallel programming

--- Java/Adding a image using rmi/ImageClient.java
+++ Java/Adding a image using rmi/ImageClient.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.awt.Image.BufferedImage;
+import java.awt.image;
 
 import javax.imageio.ImageIO;
 
@@ -12,4 +14,19 @@ public class ImageClient
         BufferedImage img = ImageIO.read(this.getClass().getResource("butterfly.jpg"))
     }
     
+
+    /**
+         * Dividing original image
+         */
+
+    BufferedImage[] splitImages = new BufferedImage[3];
+
+    int splitHeight = img.getHeight() / 3;
+    int splitWidth = img.getWidth();
+
+    for(
+    int i = 0;i<3;i++)
+    {
+        splitImages[i] = img.getSubimage(0, i * splitHeight, splitWidth, splitHeight);
+    }
 }
