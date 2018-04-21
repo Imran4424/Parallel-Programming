2ff412feea0ae1b22abe2a9421e6722e035f9e8f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-21 21:02:34 +0600

    parallel programming

--- Java/Adding a image using rmi/ReadImage.java
+++ Java/Adding a image using rmi/ReadImage.java
@@ -56,7 +56,7 @@ public class ReadImage
         JLabel lbl = new JLabel();
         lbl.setIcon(icon);
 
-        JLabel splim1 = new JLabel(new ImageIcon());
+        JLabel splim1 = new JLabel(new ImageIcon(splitImages[0]));
 
         JFrame frame = new JFrame();
 
