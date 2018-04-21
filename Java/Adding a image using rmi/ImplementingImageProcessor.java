cd1fdd71a3f63f82b1c58a80667ea340a067e60a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-21 22:52:55 +0600

    parallel programming

---
+++ Java/Adding a image using rmi/ImplementingImageProcessor.java
@@ -0,0 +1,13 @@
+import java.rmi.server.UnicastRemoteObject;
+
+
+/**
+ * ImplementingImageProcessor
+ */
+
+
+public class ImplementingImageProcessor extends UnicastRemoteObject implements ImageProcessor
+{
+    
+    
+}
