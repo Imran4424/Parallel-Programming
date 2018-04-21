91ca854934e44c69e2191dbef05749d89fe19c25
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-21 18:31:07 +0600

    parallel programming

--- Java/Adding a image using rmi/ReadImage.java
+++ Java/Adding a image using rmi/ReadImage.java
@@ -41,7 +41,7 @@ public class ReadImage
 
         for(int i = 0;i < 3;i++)
         {
-            splitImages[i] = img.getSubimage(splitWidth, splitHeight, splitWidth, splitHeight);
+            splitImages[i] = img.getSubimage(0, 0, 0, 0);
 
 /*             try 
             {
