af2f801ea8d07219be85188ade32e7135d442ca6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-22 00:36:03 +0600

    parallel programming

--- Java/Adding a image using rmi/ImageClient.java
+++ Java/Adding a image using rmi/ImageClient.java
@@ -1,15 +1,15 @@
+import java.io.IOException;
 
-
-
+import javax.imageio.ImageIO;
 
 /**
  * ImageClient
  */
 public class ImageClient 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     {
-        
+        BufferedImage img = ImageIO.read(this.getClass().getResource("butterfly.jpg"))
     }
     
 }
