548462a1ab52e3f2f350b1e02fd6f87142d1c801
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-03 20:02:51 +0600

    openmp

--- openmp (c and c++)/setting_the_num_threads.cpp
+++ openmp (c and c++)/setting_the_num_threads.cpp
@@ -3,7 +3,14 @@
 
 int main(int argc, char const *argv[])
 {
-	omp_set
+	omp_set_num_threads(3)
+
+	#pragma omp parallel
+	{
+		id = omp_get_thread_num();
+
+		printf("I am a Progammer(%d)\n", id);
+	}
 
 	return 0;
 }
