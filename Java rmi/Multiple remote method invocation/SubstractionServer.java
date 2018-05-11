f31e895e7d7a544b889305c60cf481d42ce94edd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-11 19:37:40 +0600

    multiple method invocation java rmi

--- Java rmi/Multiple remote method invocation/SubstractionServer.java
+++ Java rmi/Multiple remote method invocation/SubstractionServer.java
@@ -23,7 +23,8 @@ public class SubstractionServer
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         new SubstractionServer();
     }
     
