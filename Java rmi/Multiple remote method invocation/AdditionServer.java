995375c0b54992cb429caa79c853c1316826bccc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-11 19:20:43 +0600

    multiple method invocation java rmi

--- Java rmi/Multiple remote method invocation/AdditionServer.java
+++ Java rmi/Multiple remote method invocation/AdditionServer.java
@@ -13,7 +13,7 @@ public class AdditionServer
         {
             Calcutalor obj = new Addition();
 
-            String url = "";
+            String url = "rmi://";
         } 
         catch (Exception e) 
         {
