a451fa712ef3e6f073d03baa8d13f99dccc9828a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-11 19:17:21 +0600

    multiple method invocation java rmi

--- Java rmi/Multiple remote method invocation/AdditionServer.java
+++ Java rmi/Multiple remote method invocation/AdditionServer.java
@@ -1,10 +1,12 @@
 import java.rmi.Naming;
 
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
+
 /**
  * AdditionServer
  */
 public class AdditionServer 
 {
-
+    public CalculatorServer
     
 }
