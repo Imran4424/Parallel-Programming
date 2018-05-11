db4356307e1aeb11c7adce01106d8a0969573b6f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-11 21:28:58 +0600

    multiple method invocation java rmi

--- Java rmi/Multiple remote method invocation/MultipicationServer.java
+++ Java rmi/Multiple remote method invocation/MultipicationServer.java
@@ -21,7 +21,7 @@ import java.rmi.Naming;
         {
             //TODO: handle exception
 
-            System.out.println("");
+            System.out.println("Trouble " + e);
         }
     }
     
