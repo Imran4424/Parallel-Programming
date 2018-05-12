e143b2b7cb544331a65a1197c08e7dc95077c0e7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-13 03:26:17 +0600

    histogram

--- openmp (c and c++)/Histogram/histogram_serial.cpp
+++ openmp (c and c++)/Histogram/histogram_serial.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <omp.h>
+#include <map>
+using namespace std;
 
 
 int main(int argc, char const *argv[])
 {
-	int data[] = {1,0,3,2,5,2,3,4,1,0,2,1,3,4,5,5,5,3,2,1,1,1,0,0,0,2,1,0,1,2,3,5,5,5,3,5,3,5,0,5,5};
+	int data[] = {1,0,3,2,5,2,3,4,1,0,2,1,3,4,5,5,5,3,2,1,1,1,0,0,0,2,1,0,1,2,3,5,5,5,3,5,3,5,0,5,5,4,5,4,4,5};
+
+	map<int,int> 
+
+
 	return 0;
 }
