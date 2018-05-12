e242ea42d44e23453091a57f36e3ad0a1cffd653
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-13 03:37:25 +0600

    histogram

--- openmp (c and c++)/Histogram/histogram_serial.cpp
+++ openmp (c and c++)/Histogram/histogram_serial.cpp
@@ -8,7 +8,9 @@ int main(int argc, char const *argv[])
 {
 	int data[] = {1,0,3,2,5,2,3,4,1,0,2,1,3,4,5,5,5,3,2,1,1,1,0,0,0,2,1,0,1,2,3,5,5,5,3,5,3,5,0,5,5,4,5,4,4,5};
 
-	map<int,int> 
+	map<int,int> bins;
+
+	
 
 
 	return 0;
