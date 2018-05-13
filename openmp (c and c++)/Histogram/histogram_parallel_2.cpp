00aaf303225366b31f7c42c4e7f8496661001e46
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-13 07:44:43 +0600

    histogram

--- openmp (c and c++)/Histogram/histogram_parallel_2.cpp
+++ openmp (c and c++)/Histogram/histogram_parallel_2.cpp
@@ -9,7 +9,7 @@ pthread_mutex_t key;
 
 int main(int argc, char const *argv[])
 {
-	int data[] = {1,0,3,2,5,2,3,4,1,0,2,1,3,4,5,5,5,3,2,1,1,1,0,0,0,2,1,0,1,2,3,5,5,5,3,5,3,5,0,5,5,4,5,4,4,5};
+	int data[20]; 
 
 	map<int,int> bins;
 
