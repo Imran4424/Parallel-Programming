b1c646bea3d99e4f24c0d1cc67fa26553030e180
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-14 22:12:59 +0600

    multiple method invocation

--- Java rmi/Multiple remote method invocation/Client.java
+++ Java rmi/Multiple remote method invocation/Client.java
@@ -8,7 +8,9 @@ public class Client
 {
     public static void main(String[] args) 
     {
-        int x = Integer.parseInt(args[0]);    
+        int x = Integer.parseInt(args[0]);
+        
+        int y = Integer.parseInt(s)
     }
     
 }
