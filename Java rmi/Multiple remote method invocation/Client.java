810761e4a10a70fa86f969d30f3a07bbf06d7741
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-15 08:56:53 +0600

    multiple method invocation

--- Java rmi/Multiple remote method invocation/Client.java
+++ Java rmi/Multiple remote method invocation/Client.java
@@ -35,6 +35,7 @@ public class Client
             Calculator div_obj = (Calculator) Naming.lookup(div_url);
 
             System.out.println("The addition of " + x + " and " + y + " is: " + add_obj.sum(x, y));
+            System.out.println("The addition of " + x + " and " + y + " is: " + add_obj.sum(x, y));
         }
     }
     
