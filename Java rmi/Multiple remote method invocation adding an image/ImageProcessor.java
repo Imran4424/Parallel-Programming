f32f7184f761647587831a472a3fbb4e831ef2a4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-19 07:07:41 +0600

    java rmi

--- Java rmi/Multiple remote method invocation adding an image/ImageProcessor.java
+++ Java rmi/Multiple remote method invocation adding an image/ImageProcessor.java
@@ -21,5 +21,4 @@ public interface ImageProcessor extends Remote
 
     public ImageIcon setWaterMarkOne(ImageIcon img) throws RemoteException, IOException;
     public ImageIcon setWaterMarkTwo(ImageIcon img) throws RemoteException, IOException;
-    
 }
