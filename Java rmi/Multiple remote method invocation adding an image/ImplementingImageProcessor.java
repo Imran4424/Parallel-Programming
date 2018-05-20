69bf30ca686723f5699603fe6e02f8b912b2886e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-20 18:05:57 +0600

    java rmi- multiple method invocation

--- Java rmi/Multiple remote method invocation adding an image/ImplementingImageProcessor.java
+++ Java rmi/Multiple remote method invocation adding an image/ImplementingImageProcessor.java
@@ -11,7 +11,7 @@ import java.rmi.server.UnicastRemoteObject;
 //import java.text.ParsePosition;
 
 import java.io.IOException;
-import javax.imageio.ImageIO;
+//import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 //import javax.swing.text.Position;
 //import java.io.NotSerializableException;
