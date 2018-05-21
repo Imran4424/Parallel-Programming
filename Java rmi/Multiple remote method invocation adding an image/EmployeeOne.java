9de9948b0f43f18e9f8b52c27b6228caaae94933
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-21 17:00:36 +0600

    java rmi

--- Java rmi/Multiple remote method invocation adding an image/EmployeeOne.java
+++ Java rmi/Multiple remote method invocation adding an image/EmployeeOne.java
@@ -8,7 +8,13 @@ public class EmployeeOne
 {
     public EmployeeOne()
     {
-        
+        try 
+        {
+            ImageProcessor obj = new ImplementingImageProcessor();    
+        } 
+        catch (Exception e) {
+            //TODO: handle exception
+        }
     }
     
 }
