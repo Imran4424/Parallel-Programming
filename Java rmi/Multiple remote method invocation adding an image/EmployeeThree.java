3c99912b190562df450252e21f0d833858991398
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-21 17:35:56 +0600

    java rmi

--- Java rmi/Multiple remote method invocation adding an image/EmployeeThree.java
+++ Java rmi/Multiple remote method invocation adding an image/EmployeeThree.java
@@ -10,7 +10,9 @@ public class EmployeeThree
     {
         try 
         {
+            ImageProcessor obj = new ImplementingImageProcessor();
             
+            String url = "rmi://localhost:1099/EmployeeThreeService";
         } 
         catch (Exception e) 
         {
