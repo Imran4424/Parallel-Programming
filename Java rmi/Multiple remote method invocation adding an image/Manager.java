5716b14b74348d1194997fe4978a078db903848b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-21 17:51:01 +0600

    java rmi

--- Java rmi/Multiple remote method invocation adding an image/Manager.java
+++ Java rmi/Multiple remote method invocation adding an image/Manager.java
@@ -1,12 +1,11 @@
-
-
+import java.io.IOException;
 
 /**
  * Manager
  */
 public class Manager 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
     {
         
     }
