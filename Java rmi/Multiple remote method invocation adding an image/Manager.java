18b8d8ddbba337592a6f90a4feebce60550f68b4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-22 10:19:27 +0600

    java rmi

--- Java rmi/Multiple remote method invocation adding an image/Manager.java
+++ Java rmi/Multiple remote method invocation adding an image/Manager.java
@@ -20,6 +20,22 @@ public class Manager
         */
 
         BufferedImage readImage = ImageIO.read(this.getClass().getResource("butterfly.jpg"));
+
+        /**
+         * Dividing the original Image 
+        */
+        
+        BufferedImage[] splitImages = new BufferedImage[3];
+
+        int splitHeight = readImage.getHeight()/3;
+        int splitWidth = readImage.getWidth();
+
+        for (int i = 0; i < 3; i++) 
+        {
+            splitImages[i] = readImage.getSubimage(0, i*splitHeight, splitWidth, splitHeight);
+        }
+
+
         
     }
     
