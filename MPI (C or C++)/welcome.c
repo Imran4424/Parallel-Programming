7aa8268a78dfb7e70f8019912daa5a4199d1a844
Imran4424 <akashganga.programming.534363@gmail.com>
2018-08-07 17:23:05 +0600

    MPI

--- MPI (C or C++)/welcome.c
+++ MPI (C or C++)/welcome.c
@@ -7,9 +7,11 @@ const int maxString = 100;
 int main(int argc, char const *argv[])
 {
 	char welcome[maxString];
-	int total_process;
-	int my_rank;
+	int total_process;        // total number of process
+	int my_rank;             // process rank
 
-	
+	MPI
+
+
 	return 0;
 }
