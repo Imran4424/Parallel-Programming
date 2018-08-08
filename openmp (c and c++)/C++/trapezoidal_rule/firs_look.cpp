3f8f7e504f4611f60db7f7d89aec8b372758d2d6
Imran4424 <akashganga.programming.534363@gmail.com>
2018-08-09 01:51:40 +0600

    openmp

--- openmp (c and c++)/C++/trapezoidal_rule/firs_look.cpp
+++ openmp (c and c++)/C++/trapezoidal_rule/firs_look.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	double global_result = 0;
+	double a,b;
+	int n, thread_count;
+
+	thread_count = strtol(argv[1], NULL, 10);
+
+	cout<<"enter the values of a b n"<<endl;
+
+	cin>>a>>b>>n;
 	
+
 	return 0;
 }
