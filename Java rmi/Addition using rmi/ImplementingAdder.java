a604d26074427332e804d7c995e675b3bcb6d187
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-09 23:53:09 +0600

    parallel programming

--- Java rmi/Addition using rmi/ImplementingAdder.java
+++ Java rmi/Addition using rmi/ImplementingAdder.java
@@ -12,6 +12,19 @@ public class ImplementingAdder extends UnicastRemoteObject implements Adder
 	public ImplementingAdder() throws RemoteException
 	{
 		super();
+
+		/**
+          * super keyword use to called parent constructor
+          * super can be used to do more specific work like
+          * 
+          * super(arguments) to called parent constructor who accepts arguments
+          *
+          * super can also use to called parent class method that is  - super.aMethod()
+          *
+          * and we all know that what constructor is, constructure initialize the object values with given values
+          *
+          * for default constructors, the object values will be initialized with default values
+          */
 	}
 
 
